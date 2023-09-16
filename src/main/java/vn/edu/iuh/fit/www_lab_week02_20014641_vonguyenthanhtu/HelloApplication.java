af055de2643f5a2bfc2262ae1594d18f9d120185
ttus612 <thanhtu6122002@gmail.com>
2023-09-16 18:29:23 +0700

    first commit www week2

---
+++ src/main/java/vn/edu/iuh/fit/www_lab_week02_20014641_vonguyenthanhtu/HelloApplication.java
@@ -0,0 +1,9 @@
+package vn.edu.iuh.fit.www_lab_week02_20014641_vonguyenthanhtu;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/vn/edu/iuh/fit/www_lab_week02_20014641_vonguyenthanhtu/HelloResource.java
@@ -0,0 +1,14 @@
+package vn.edu.iuh.fit.www_lab_week02_20014641_vonguyenthanhtu;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
